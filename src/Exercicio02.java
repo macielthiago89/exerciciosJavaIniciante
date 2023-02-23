f7789cad028ed964b2411cfbc1e92ba98d6bdcc5
Thiago Maciel Andrade <maciel.thiago@gmail.com>
2023-02-23 18:31:36 -0300

    versão230223

--- src/Exercicio02.java
+++ src/Exercicio02.java
@@ -4,11 +4,13 @@ public class Exercicio02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
-
-
-
+        int valor1, valor2, result;
+        System.out.println("Digite o primeiro valor: ");
+        valor1 = sc.nextInt();
+        System.out.print("Digite o segundo valor: ");
+        valor2 = sc.nextInt();
+        result = valor1 + valor2;
+        System.out.println("Valor da soma é = " + result);
         sc.close();
-    }
-    
+    }    
 }
